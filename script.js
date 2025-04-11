d918eecdf307daab28a11841e436b820c5cedb52
LukasRandecker <randeckerlukas@gmail.com>
2025-04-11 12:20:29 +0200

    add recherche page and fill with content

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+
+  document.querySelectorAll('.flip-card').forEach(card => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('flipped');
+    });
+  });

99e84f0c53790b3cbeeef246c53dc987592938d2
LukasRandecker <randeckerlukas@gmail.com>
2025-07-19 13:27:51 +0200

    Responsivity and content

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.getElementById('hamburger');
     const nav = document.getElementById('nav');
-  
+
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('open');
       });

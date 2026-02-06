56e3961a29ce804192fba0a61daadb882f8c5226
Lukas Randecker <randeckerlukas@gmail.com>
2026-02-06 13:26:33 +0100

    new build upload

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/gotteswut.github.io/', // Name des Repos
+  base: '/gotteswut.github.io/', // Name des Repo
 })
